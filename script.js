87a9b302e017af07ddc33aa1e9332c39bcbd3da3
Sofia Makraki <160715585+Moldedpeanuts@users.noreply.github.com>
2024-12-11 14:25:45 +0200

    Add prompt for correct user input number range

--- script.js
+++ script.js
@@ -1,14 +1,3 @@
-// Getting user input
-
-const buttonGrid = document.querySelector("#prompt");
-
-let userInput;
-
-buttonGrid.addEventListener('click', () => {
-    userInput = parseInt(prompt("Please input a grid number"));
-});
-
-
 // Creating a 16 x 16 grid of squares
 
 for (let i = 0; i < 256; i++) {
@@ -40,3 +29,16 @@ for (let i = 0; i < 256; i++) {
 
 }
 
+
+// Getting user input
+
+const buttonGrid = document.querySelector("#prompt");
+
+let userInput;
+
+buttonGrid.addEventListener('click', () => {
+    userInput = parseInt(prompt("Please input a grid number"));
+    if (userInput < 0 || userInput > 100) {
+        userInput = parseInt(prompt("Please enter an integer between 0 and 100"));
+    }
+});
