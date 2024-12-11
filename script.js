cf0bbb914c1673e1d97bb537b43a30d5c41169f8
Sofia Makraki <160715585+Moldedpeanuts@users.noreply.github.com>
2024-12-11 12:02:32 +0200

    Style square

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 const square = document.createElement("div");
 const container = document.querySelector(".container");
 
+square.style.width = "100px";
+square.style.height = "100px";
+square.style.background = "white";
+square.style.border = "1px solid black";
+
 
