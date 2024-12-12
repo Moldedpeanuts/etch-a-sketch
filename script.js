4769f331289264079f0a9fc1517d6fa4eb928063
Sofia Makraki <160715585+Moldedpeanuts@users.noreply.github.com>
2024-12-12 12:56:56 +0200

    Add switch statement to handle edge user input cases

--- script.js
+++ script.js
@@ -34,11 +34,16 @@ for (let i = 0; i < 256; i++) {
 
 const buttonGrid = document.querySelector("#prompt");
 
-let userInput;
 
 buttonGrid.addEventListener('click', () => {
-    userInput = parseInt(prompt("Please input a grid number"));
-    if (userInput < 0 || userInput > 100) {
-        userInput = parseInt(prompt("Please enter an integer between 0 and 100"));
+    let userInput = Number(prompt('Please enter an integer for appropriate grid'));
+
+    switch(true) {
+        case (userInput < 0):
+        case (userInput > 100):
+        case (isNaN(userInput)):
+        case (typeof userInput === "number" && !Number.isInteger(userInput)):
+            alert("Please input a valid integer number from 0 to 100 to change the grid");
+            break;
     }
 });
