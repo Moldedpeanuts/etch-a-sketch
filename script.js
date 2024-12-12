6474484d4cc933d5a4e0e77ef7032c96aff99c66
Sofia Makraki <160715585+Moldedpeanuts@users.noreply.github.com>
2024-12-12 13:00:50 +0200

    Change prompt text

--- script.js
+++ script.js
@@ -36,7 +36,7 @@ const buttonGrid = document.querySelector("#prompt");
 
 
 buttonGrid.addEventListener('click', () => {
-    let userInput = Number(prompt('Enter a value to change squares per side'));
+    let userInput = Number(prompt('Please enter a number to change squares per side'));
 
     switch(true) {
         case (userInput < 0):
